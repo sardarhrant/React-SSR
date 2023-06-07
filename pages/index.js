fdcefc4c4f9dfce51913c35024b2a32db7c387b7
Hrant Sardaryan <hrant_sardaryan@epam.com>
2023-06-07 17:01:41 +0400

    Init React SSR

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './index.module.css'
+import Card from './Card'
+import data from './API/data.json'
+
+export default class Index extends React.Component {
+    static async getInitialProps () {
+        return { cards: data }
+    }
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <header className={styles.header}>
+                    <img className={styles.logo} alt="logo" src="/logo.png" />
+                </header>
+                <div className={styles.grid}>
+                    {
+                        this.props.cards.map((card) => <Card key={card.id} />)
+                    }
+                </div>
+            </div>
+        )
+    }
+    
+}
